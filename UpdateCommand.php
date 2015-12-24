18f0f1c9f664ccfd5d754a3b43343bb6ff393fb8
Pieter Meyvaert <pieter.meyvaert@studioemma.com>
2015-12-24 14:51:55 +0100

    Only update dns records when external ip has been changed. Also, added basic logging about this

--- UpdateCommand.php
+++ UpdateCommand.php
@@ -1,9 +1,12 @@
 <?php
 
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Yaml\Dumper;
 
 /**
  * Class UpdateCommand
@@ -41,6 +44,9 @@ class UpdateCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $log = new Logger('dns-updater');
+        $log->pushHandler(new StreamHandler(__DIR__ . '/application.log', Logger::INFO));
+
         if ($input->hasArgument('name') && !empty($input->getArgument('name'))) {
             $names = [$input->getArgument('name')];
         } else {
@@ -50,22 +56,31 @@ class UpdateCommand extends Command
         $externalIp = $this->getExternalIp();
         $output->writeln("<info>External IP queried: " . $externalIp . '</info>');
 
-        foreach ($names as $name) {
-            if (!empty($name)) {
-                $response = $this->removeDnsRecord($name);
-                if ($response) {
-                    $output->writeln('<info>Removed dns record: ' . $name . '</info>');
+        if ($this->hasIpChanged($externalIp)) {
+            $this->storeIp($externalIp);
+            $output->writeln(sprintf('<info>External ip %s has been changed, starting dns update</info>', $externalIp));
+            $log->addInfo(sprintf('External ip %s has been changed, starting dns update', $externalIp));
 
-                    $this->addDnsRecord($name, $externalIp);
+            foreach ($names as $name) {
+                if (!empty($name)) {
+                    $response = $this->removeDnsRecord($name);
                     if ($response) {
-                        $output->writeln('<info>Added dns record: ' . $name . ' with ip ' . $externalIp . '</info>');
+                        $output->writeln('<info>Removed dns record: ' . $name . '</info>');
+
+                        $this->addDnsRecord($name, $externalIp);
+                        if ($response) {
+                            $output->writeln('<info>Added dns record: ' . $name . ' with ip ' . $externalIp . '</info>');
+                        } else {
+                            $output->writeln('<error>Error while removing dns record: ' . $name . '</error>');
+                        }
                     } else {
                         $output->writeln('<error>Error while removing dns record: ' . $name . '</error>');
                     }
-                } else {
-                    $output->writeln('<error>Error while removing dns record: ' . $name . '</error>');
                 }
             }
+        } else {
+            $output->writeln(sprintf("<info>External ip %s matches stored ip address %s, skipping dns update</info>",
+                $externalIp, $this->getStoredIp()));
         }
     }
 
@@ -85,25 +100,56 @@ class UpdateCommand extends Command
     }
 
     /**
-     * @return \DirectAdmin\DirectAdmin|null
-     */
-    protected function getConnection()
-    {
-        if (is_null($this->connection)) {
-            $this->connection = new \DirectAdmin\DirectAdmin();
-            $this->connection->connect($this->getConfig('da_hostname'), $this->getConfig('da_port'));
-            $this->connection->set_login($this->getConfig('da_username'), $this->getConfig('da_password'));
-            $this->connection->set_method('get');
-        }
-        return $this->connection;
-    }
-
-    /**
      * @return string
      */
     protected function getExternalIp()
     {
         return file_get_contents($this->getConfig('external_ip_check_url'));
+    }
+
+    /**
+     * @param $currentIp
+     * @return bool
+     */
+    protected function hasIpChanged($currentIp)
+    {
+        $hasChanged = true;
+        $storedIp = $this->getStoredIp();
+
+        if ($storedIp) {
+            if ($storedIp === $currentIp) {
+                $hasChanged = false;
+            }
+        }
+
+        return $hasChanged;
+    }
+
+    /**
+     * @return bool
+     */
+    protected function getStoredIp()
+    {
+        $ip = false;
+        if (file_exists(__DIR__ . '/ip.yml')) {
+            $ipConfig = \Symfony\Component\Yaml\Yaml::parse(__DIR__ . '/ip.yml');
+            if (!empty($ipConfig['ip'])) {
+                $ip = $ipConfig['ip'];
+            }
+        }
+
+        return $ip;
+    }
+
+    /**
+     * @param $ip
+     */
+    protected function storeIp($ip)
+    {
+        $dumper = new Dumper();
+        $yaml = $dumper->dump(['ip' => $ip]);
+
+        file_put_contents(__DIR__ . '/ip.yml', $yaml);
     }
 
     /**
@@ -117,6 +163,20 @@ class UpdateCommand extends Command
         $response = $this->getConnection()->fetch_body();
         parse_str($response, $output);
         return !boolval($output['error']);
+    }
+
+    /**
+     * @return \DirectAdmin\DirectAdmin|null
+     */
+    protected function getConnection()
+    {
+        if (is_null($this->connection)) {
+            $this->connection = new \DirectAdmin\DirectAdmin();
+            $this->connection->connect($this->getConfig('da_hostname'), $this->getConfig('da_port'));
+            $this->connection->set_login($this->getConfig('da_username'), $this->getConfig('da_password'));
+            $this->connection->set_method('get');
+        }
+        return $this->connection;
     }
 
     /**
